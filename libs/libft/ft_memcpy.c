b21cb75f0d88880664dfdcc6a9f84d7b6b981665
Yulia Ioffe <yumioffe@gmail.com>
2024-10-24 12:07:19 +0100

    add protections to memmove

--- libs/libft/ft_memcpy.c
+++ libs/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 18:51:33 by yioffe            #+#    #+#             */
-/*   Updated: 2024/10/15 14:13:49 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/10/24 12:07:02 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,18 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*p_src;
 	size_t			i;
 
-	p_dest = (unsigned char *) dest;
-	p_src = (unsigned char *) src;
+	p_dest = (unsigned char *)dest;
+	p_src = (unsigned char *)src;
 	if (p_dest == NULL || p_src == NULL)
+		return (NULL);
+	if ((uintptr_t)p_dest + n < (uintptr_t)p_dest || (uintptr_t)p_src
+		+ n < (uintptr_t)p_src)
 		return (NULL);
 	i = 0;
 	while (i < n)
 	{
 		p_dest[i] = p_src[i];
-		i ++;
+		i++;
 	}
 	return (dest);
 }
