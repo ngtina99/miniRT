9ddc72b7a5170ec78fc6aaafab4add50965ce227
Yulia Ioffe <yioffe@gmail.com>
2024-10-31 13:59:10 +0000

    fix shades

--- srcs/raytracing/ray_trace.c
+++ srcs/raytracing/ray_trace.c
@@ -12,17 +12,26 @@
 
 #include "../../includes/minirt.h"
 
+void	save_hit_values(t_object_hit *object_hit, t_vec3d hit_point, int type, int color, int index)
+{
+	object_hit->hit_point = hit_point;
+	object_hit->object_type = type;
+	object_hit->object_color = color;
+	object_hit->object_index = index;
+}
+
 // Function to find the closest intersection with any object (spheres and planes)
-int	find_closest_object(t_data *data, t_vec3d origin, t_vec3d direction, t_vec3d *closest_hit_point, int *object_type, int *object_index)
+bool	find_closest_object(t_data *data, t_vec3d origin, t_vec3d direction, t_object_hit *closest_hit)
 {
 	int i ;
 	t_vec3d hit_point;
-	int hit; // Flag to check if any intersection occurs
+	bool hit; // Flag to check if any intersection occurs
 	float min_distance;
 	float distance; //TODO maybe set up to 0 if it shows uninitialised value
+	int color;
 
 	i = 0;
-	hit = 0;
+	hit = false;
 	min_distance = INFINITY; // Initially set the minimum distance to infinity
 	while (i < data->sphere_count)
 	{
@@ -32,10 +41,9 @@ int	find_closest_object(t_data *data, t_vec3d origin, t_vec3d direction, t_vec3d
 			if (distance < min_distance)
 			{
 				min_distance = distance;
-				*closest_hit_point = hit_point;
-				*object_type = SPHERE;
-				*object_index = i;
-				hit = 1;
+				color = convert_rgb_to_int(data->spheres[i].color);
+				save_hit_values(closest_hit, hit_point, SPHERE, color, i);
+				hit = true;
 			}
 		}
 		i++;
@@ -49,10 +57,9 @@ int	find_closest_object(t_data *data, t_vec3d origin, t_vec3d direction, t_vec3d
             if (distance < min_distance)
             {
                 min_distance = distance;
-                *closest_hit_point = hit_point;
-                *object_type = PLANE;
-                *object_index = i;
-                hit = 1;
+				color = convert_rgb_to_int(data->planes[i].color);
+				save_hit_values(closest_hit, hit_point, PLANE, color, i);
+                hit = true;
             }
         }
         i++;
@@ -66,37 +73,34 @@ int	find_closest_object(t_data *data, t_vec3d origin, t_vec3d direction, t_vec3d
 			if (distance < min_distance)
 			{
 				min_distance = distance;
-				*closest_hit_point = hit_point;
-				*object_type = CYLINDER;
-				*object_index = i;
-				hit = 1;
-				}
+				color = convert_rgb_to_int(data->cylinders[i].color);
+				save_hit_values(closest_hit, hit_point, CYLINDER, color, i);
+				hit = true;
+			}
 		}
-		if (ray_cylinder_top(data->cylinders[i], origin, direction, &hit_point))
-		{
-            distance = calculate_distance(origin, hit_point);
-            if (distance < min_distance)
-            {
-                min_distance = distance;
-                *closest_hit_point = hit_point;
-                *object_type = CY_TOP;
-                *object_index = i;
-                hit = 1;
-            }
-        }
+		// if (ray_cylinder_top(data->cylinders[i], origin, direction, &hit_point))
+		// {
+        //     distance = calculate_distance(origin, hit_point);
+        //     if (distance < min_distance)
+        //     {
+        //         min_distance = distance;
+		// 		color = convert_rgb_to_int(data->cylinders[i].color);
+        //         save_hit_values(closest_hit, hit_point, CY_TOP, color, i);
+        //         hit = true;
+        //     }
+        // }
 
-        if (ray_cylinder_bottom(data->cylinders[i], origin, direction, &hit_point))
-        {
-            distance = calculate_distance(origin, hit_point);
-            if (distance < min_distance)
-            {
-                min_distance = distance;
-                *closest_hit_point = hit_point;
-                *object_type = CY_BOTTOM;
-                *object_index = i;
-                hit = 1;
-            }
-        }
+        // if (ray_cylinder_bottom(data->cylinders[i], origin, direction, &hit_point))
+        // {
+        //     distance = calculate_distance(origin, hit_point);
+        //     if (distance < min_distance)
+        //     {
+        //         min_distance = distance;
+		// 		color = convert_rgb_to_int(data->cylinders[i].color);
+        //         save_hit_values(closest_hit, hit_point, CY_BOTTOM, color, i);
+        //         hit = true;
+        //     }
+        // }
         i++;
     }
     return (hit); // Return whether any intersection was found
@@ -119,14 +123,26 @@ t_vec3d sphere_normal(t_sphere sphere, t_vec3d hit_point)
 //not good yet!
 t_vec3d cylinder_normal(t_cylinder cylinder, t_vec3d hit_point)
 {
+    // Vector from cylinder center to hit point
     t_vec3d center_to_hit = subtract_vector(hit_point, cylinder.center);
+    // Project the vector onto the cylinder axis to find the closest point on the axis
     float projection_length = dot_product(center_to_hit, cylinder.axis);
+    
+    // If the intersection point is on the bottom cap
+    if (projection_length <= 0) {
+        return normalize(scale_vector(cylinder.axis, -1)); // Normal points inward for bottom cap
+    }
+    // If the intersection point is on the top cap
+    else if (projection_length >= cylinder.height) {
+        return normalize(cylinder.axis); // Normal points outward for top cap
+    }
 
+    // Otherwise, calculate the normal for the cylindrical surface
     t_vec3d closest_point_on_axis = add_vector(cylinder.center, scale_vector(cylinder.axis, projection_length));
-
     t_vec3d normal = subtract_vector(hit_point, closest_point_on_axis);
     return normalize(normal);
 }
+
 
 
 
@@ -162,35 +178,34 @@ void	ray_trace(t_data *data, int x, int y, int screen_width, int screen_height)
 	direction = normalize(direction);
 
     // Find the closest intersection among all objects (spheres and planes)
+	t_object_hit closest_hit;
 	t_vec3d closest_hit_point; // TODO is it needed later?
-	int object_type;
-	int object_index;
 	int color_code;
 	int ambient_color;
-	if (find_closest_object(data, origin, direction, &closest_hit_point, &object_type, &object_index))
+	if (find_closest_object(data, origin, direction, &closest_hit))
 	{
 		t_vec3d normal;
 
-		if (object_type == SPHERE)
-			normal = sphere_normal(data->spheres[object_index], closest_hit_point);
-		else if (object_type == PLANE)
-			normal = data->planes[object_index].normal;
-		else if (object_type == CYLINDER)
-			normal = cylinder_normal(data->cylinders[object_index], closest_hit_point);
-		t_vec3d light_dir = normalize(subtract_vector(data->light.position, closest_hit_point));
+		if (closest_hit.object_type == SPHERE)
+			normal = sphere_normal(data->spheres[closest_hit.object_index], closest_hit.hit_point);
+		else if (closest_hit.object_type == PLANE)
+			normal = data->planes[closest_hit.object_index].normal;
+		else if (closest_hit.object_type == CYLINDER)
+			normal = cylinder_normal(data->cylinders[closest_hit.object_index], closest_hit.hit_point);
+		t_vec3d light_dir = normalize(subtract_vector(data->light.position, closest_hit.hit_point));
 
-		// calculating light intensity. can we use fmax()?
-		float light_intensity = fmax(0.0, dot_product(normal, light_dir)) * data->light.brightness;
+		// calculating light intensity. are we allowed to use fmax()?
+		float ambient_intensity = data->ambient.ratio;
+		float diffuse_intensity = fmax(0.0f, dot_product(normal, light_dir)) * data->light.brightness;
+
+		// Combine ambient and diffuse components for the final intensity
+		float light_intensity = ambient_intensity + diffuse_intensity;
+
+		// Clamp the final intensity to [0, 1] to avoid over-brightening
+		light_intensity = fmin(light_intensity, 1.0f);
 
 		// adding darkening to base light
-		int base_color;
-		if (object_type == SPHERE)
-			base_color = convert_rgb_to_int(data->spheres[object_index].color);
-		else if (object_type == PLANE)
-			base_color = convert_rgb_to_int(data->planes[object_index].color);
-		else
-			base_color = convert_rgb_to_int(data->cylinders[object_index].color);
-
+		int base_color = closest_hit.object_color;
 		color_code = apply_shading(base_color, light_intensity);
 		my_mlx_pixel_put(data->img, x, y, color_code);
 	}
