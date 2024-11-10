9f5fa7604f4c9bc9958570bf6066fa3213b353ab
Yulia Ioffe <yumioffe@gmail.com>
2024-11-10 20:47:50 +0000

    .h file

--- includes/minirt.h
+++ includes/minirt.h
@@ -1,14 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
+/*   Created: 2024/11/10 20:44:40 by yioffe            #+#    #+#             */
+/*   Updated: 2024/11/10 20:44:40 by yioffe           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINIRT_H
 # define MINIRT_H
 
-# include "parsing.h"
-
 # include "../libs/libft/libft.h"
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
+# include "parsing.h"
 # include <math.h>
 # include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # ifdef __APPLE__
 #  include "../libs/minilibx-mac/mlx.h"
@@ -28,23 +42,22 @@
 #  define RIGHT 65363
 # endif
 
-
 # define WIDTH 1200
 # define HEIGHT 1200
 
-#define PATH_LEN 9
-//COLORCODES
-#define RESET "\033[0m"
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN "\033[36m"
-#define WHITE "\033[37m"
-#define BLACK "\033[30m"
+# define PATH_LEN 9
+// COLORCODES
+# define RESET "\033[0m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define WHITE "\033[37m"
+# define BLACK "\033[30m"
 
-//KEYCODES
+// KEYCODES
 # define SPACE 32
 # define ONE 49
 # define TWO 50
@@ -70,8 +83,8 @@
 // # define RIGHT 65363
 // # define LEFT 65361
 
-//EVENTCODES
-enum	key_codes
+// EVENTCODES
+enum e_key_codes
 {
 	ON_KEYDOWN = 2,
 	ON_KEYUP = 3,
@@ -84,7 +97,7 @@ enum	key_codes
 
 # define EPSILON 1e-4
 
-enum	object_type
+enum e_object_type
 {
 	SPHERE,
 	PLANE,
@@ -95,98 +108,105 @@ enum	object_type
 
 typedef struct s_data t_data;
 
-typedef struct	s_graphic {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-	t_data	*data;
-}				t_graphic;
+typedef struct s_graphic
+{
+	void *mlx_ptr;
+	void *win_ptr;
+	void *img;
+	char *addr;
+	int bits_per_pixel;
+	int line_length;
+	int endian;
+	t_data *data;
+} t_graphic;
 
-typedef struct s_data {
-    t_ambient ambient;      // Single ambient light
-    t_camera camera;             // Single camera
-    t_light light;               // Single light
-    
-    t_sphere *spheres;           // Dynamic array of spheres
-    int sphere_count;          // Number of spheres
-	int	sphere_capacity;
+typedef struct s_data
+{
+	t_ambient ambient; // Single ambient light
+	t_camera camera;   // Single camera
+	t_light light;     // Single light
 
-    t_plane *planes;             // Dynamic array of planes
-    int plane_count;           // Number of planes
-	int	plane_capacity;
+	t_sphere *spheres; // Dynamic array of spheres
+	int sphere_count;  // Number of spheres
+	int sphere_capacity;
 
-    t_cylinder *cylinders;       // Dynamic array of cylinders
-    int cylinder_count;        // Number of cylinders
-	int	cylinder_capacity;
+	t_plane *planes; // Dynamic array of planes
+	int plane_count; // Number of planes
+	int plane_capacity;
+
+	t_cylinder *cylinders; // Dynamic array of cylinders
+	int cylinder_count;    // Number of cylinders
+	int cylinder_capacity;
 
 	// TODO: we probably won't need this at all
-	int	red;//init to 0
-	int	green;
-	int	blue;
+	int red; // init to 0
+	int green;
+	int blue;
 
-	t_graphic	*img;
+	t_graphic *img;
 
 } t_data;
 
 typedef struct s_ray
 {
-    t_vec3d origin;    // Starting point of the ray (camera position)
-    t_vec3d direction; // Direction the ray is traveling
-}   t_ray;
+	t_vec3d origin;    // Starting point of the ray (camera position)
+	t_vec3d direction; // Direction the ray is traveling
+} t_ray;
 
 typedef struct s_object_hit
 {
 	t_vec3d hit_point;
 	int object_type;
 	int object_color;
-	int	object_index;
+	int object_index;
 } t_object_hit;
 
 typedef struct s_discr_util
 {
-	float	a;
-	float	b;
-	float	c;
-	float	discriminant;
+	float a;
+	float b;
+	float c;
+	float discriminant;
 } t_discr_util;
 
 typedef struct s_cyl_intersection_util
 {
-	t_vec3d	cylinder_axis;
-	t_vec3d	ray_dir_perp;
-	t_vec3d	oc_perp;
-	float	ray_dir_parallel;
-	float	oc_parallel;
-}			t_cyl_intersection_util;
+	t_vec3d cylinder_axis;
+	t_vec3d ray_dir_perp;
+	t_vec3d oc_perp;
+	float ray_dir_parallel;
+	float oc_parallel;
+} t_cyl_intersection_util;
 
-int		open_rt(int argc, char **argv);
+int	open_rt(int argc, char **argv);
 void	init_mlx(t_data *data);
-int 	convert_rgb_to_int(t__color_rgb color);
+int	convert_rgb_to_int(t__color_rgb color);
 void	my_mlx_pixel_put(t_graphic *data, int x, int y, int color);
 void	init_scene_img(t_data *data);
 void	initialize_scene(t_data *scene);
-int		parse_scene(t_data *scene, int fd);
+int	parse_scene(t_data *scene, int fd);
 void	free_scene(t_data **scene);
-t_vec3d add_vector(t_vec3d v1, t_vec3d v2);
-t_vec3d subtract_vector(t_vec3d v1, t_vec3d v2);
+t_vec3d	add_vector(t_vec3d v1, t_vec3d v2);
+t_vec3d	subtract_vector(t_vec3d v1, t_vec3d v2);
 t_vec3d	scale_vector(t_vec3d v, float scale);
-float vector_length(t_vec3d v);
+float	vector_length(t_vec3d v);
 t_vec3d	normalize(t_vec3d v);
 t_vec3d	cross_product(t_vec3d v1, t_vec3d v2);
 float	dot_product(t_vec3d v1, t_vec3d v2);
 void	ray_trace(t_data *data, int x, int y);
 float	calculate_distance(t_vec3d point1, t_vec3d point2);
-bool	ray_sphere_intersection(t_sphere sphere, t_vec3d origin, t_vec3d direction, t_vec3d *hit_point);
-bool 	ray_plane_intersection(t_plane plane, t_vec3d origin, t_vec3d direction, t_vec3d *hit_point);
-bool	ray_cylinder_intersection(t_cylinder cylinder, t_vec3d ray_origin, t_vec3d ray_direction, t_vec3d *hit_point);
-bool	ray_cylinder_top(t_cylinder cylinder, t_vec3d ray_origin, t_vec3d ray_direction, t_vec3d *hit_point);
-bool	ray_cylinder_bottom(t_cylinder cylinder, t_vec3d ray_origin, t_vec3d ray_direction, t_vec3d *hit_point);
-int		ft_mousehooks(int button, int x, int y, t_data *img);
-int		ft_keyhooks(int keycode, t_data *img);
+bool	ray_sphere_intersection(t_sphere sphere, t_vec3d origin,
+		t_vec3d direction, t_vec3d *hit_point);
+bool	ray_plane_intersection(t_plane plane, t_vec3d origin, t_vec3d direction,
+		t_vec3d *hit_point);
+bool	ray_cylinder_intersection(t_cylinder cylinder, t_vec3d ray_origin,
+		t_vec3d ray_direction, t_vec3d *hit_point);
+bool	ray_cylinder_top(t_cylinder cylinder, t_vec3d ray_origin,
+		t_vec3d ray_direction, t_vec3d *hit_point);
+bool	ray_cylinder_bottom(t_cylinder cylinder, t_vec3d ray_origin,
+		t_vec3d ray_direction, t_vec3d *hit_point);
+int	ft_mousehooks(int button, int x, int y, t_data *img);
+int	ft_keyhooks(int keycode, t_data *img);
 t_vec3d	setup_camera(t_data *data, int x, int y);
 float	calculate_nearest_inters_p(float a, float b, float discriminant);
 bool	calculate_quadratic_coefficients(t_discr_util *discr,
@@ -198,7 +218,7 @@ bool	calculate_intersection_point(t_vec3d ray_origin, t_vec3d ray_direction,
 bool	within_cap_radius(t_vec3d point, t_vec3d cap_center, float cap_radius);
 
 # ifdef __APPLE__
-void		mlx_destroy_display(void *mlx_ptr);
+void	mlx_destroy_display(void *mlx_ptr);
 # endif
 
 #endif
