229e484e3974026dd9401ac270f9fe39e05135ed
Yulia Ioffe <yumioffe@gmail.com>
2024-11-19 16:43:53 +0000

    fix normi

--- srcs/parsing/init_file.c
+++ srcs/parsing/init_file.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_file.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 18:49:19 by thuy-ngu          #+#    #+#             */
-/*   Updated: 2024/11/16 01:30:33 by marvin           ###   ########.fr       */
+/*   Updated: 2024/11/19 16:34:36 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int	open_rt(int argc, char **argv)
 	int		fd;
 
 	file_name = argv[1];
-	if (ft_strlen(file_name) < 3 || ft_strcmp(file_name + ft_strlen(file_name) - 3, ".rt") != 0)
+	if (ft_strlen(file_name) < 3
+		|| ft_strcmp(file_name + ft_strlen(file_name) - 3, ".rt") != 0)
 	{
 		printf(RED "Error: The file doesn't have a .rt extension\n" RESET);
 		return (-1);
@@ -26,7 +27,8 @@ int	open_rt(int argc, char **argv)
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
 	{
-		printf(RED "Error: input is not an existing .rt file path or the file can't be opened\n" RESET);
+		printf(RED "Error: input is not an existing .rt \
+			file path or the file can't be opened\n" RESET);
 		return (-1);
 	}
 	return (fd);
